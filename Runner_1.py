e6fbe964953496f0d9b5ae656fbbfafdf880eac1
WaiLok0092 <pthz0092@leeds.ac.uk>
2026-01-20 19:01:03 +0000

    runner_message.ver0.1

---
+++ Runner_1.py
@@ -0,0 +1,9 @@
+import radio
+while True:
+    incoming = radio.receive()
+    if incoming == 'Start':
+        #code that start the timmer
+        pass
+    if incoming == 'End1':
+        #code that Stop the timmer for Runner 1
+        pass
---
+++ Starting_message.py
@@ -0,0 +1,4 @@
+import radio
+while True:
+     if button_a.was_pressed():
+        radio.send('Start') #let runner microbit start timmer
