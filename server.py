114e20fc5d73373bc05ee691930e55af85f39141
ELJANMUSTAFAYEV <lwmb0280@leeds.ac.uk>
2026-01-21 23:11:23 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
+
+import radio
 from microbit import *
+lt=list()
+time=0
+last=0
+while True:
+    
+
+   message=radio.receive()
+   try:
+       lt.append((time,int(message)))
+       last=int(message)
+   except:
+       lt.append((time,(last)))
+   sleep(1000)
+   time+=1
