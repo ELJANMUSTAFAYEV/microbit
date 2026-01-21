182e186c19a706589199cf3e58d757442c304754
WaiLok0092 <pthz0092@leeds.ac.uk>
2026-01-21 14:40:58 +0000

    ver0.2

--- Stopwatch1.py
+++ Stopwatch1.py
@@ -4,7 +4,7 @@ while True:
     incoming = radio.receive()
     if incoming == 'Start':
         start_time = running_time()
-    if incoming == 'End':
+    if incoming == 'End1':
         elapsed_ms = running_time() - start_time
         Final_time=(elapsed_ms // 1000)
 
