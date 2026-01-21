318a8fa2038b85a8b18e30f74329c6c75954796e
uni-rhysec <rfrx0712@leeds.ac.uk>
2026-01-21 14:28:29 +0000

    countdown code

---
+++ countdown.py
@@ -0,0 +1,28 @@
+from microbit import *
+import radio
+import music
+import time
+
+def StartCountdown():
+    music.play(['c5:1'])
+    display.show("3")
+    time.sleep(1)
+        
+    music.play(['c5:1'])
+    display.show("2")
+    time.sleep(1)
+        
+    music.play(['c5:1'])
+    display.show("1")
+    time.sleep(1)
+        
+    music.play(['e5:3'])
+    display.show("GO")
+
+
+while True:
+    incoming = radio.receive()
+    if incoming == 'TEST':
+        StartCountdown()
+        
+        
