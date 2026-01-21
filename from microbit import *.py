2f97a03dfadd215bc4d838053803709e04daeb65
omar161207 <pvtg0844@leeds.ac.uk>
2026-01-21 12:38:26 +0000

    progress

--- from microbit import *.py
+++ from microbit import *.py
@@ -3,12 +3,12 @@ from microbit import *
 DISTANCE = 50  # meters
 
 # real_val comes from the stopwatch
-# time_sec = real_value   
+# time_sec = real_value1   
 time_sec = 7.9
 
 if time_sec > 0:
-    speed_mps = DISTANCE / time_sec        # meters per second
-    speed_kmh = speed_mps * 3.6             # convert to km/h
+    speed_mps1 = DISTANCE / time_sec        # meters per second
+    speed_kmh1 = speed_mps1 * 3.6             # convert to km/h
 
     display.scroll(str(round(speed_kmh, 1)) + " kmh")
 else:
