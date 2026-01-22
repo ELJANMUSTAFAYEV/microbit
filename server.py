d460c5c8ecf8f321d1aee0f963ad59658ccb18fa
ELJANMUSTAFAYEV <lwmb0280@leeds.ac.uk>
2026-01-22 03:04:52 +0000

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from microbit import *
 lt=list()
 time=0
 last=0
+radio.on()
 while True:
     
 
@@ -15,3 +16,34 @@ while True:
        lt.append((time,(last)))
    sleep(1000)
    time+=1
+
+import matplotlib.pyplot as plt
+import random 
+plt.ion()
+fig=plt.figure()
+
+ax=fig.add_subplot(111)
+ax.set_ylabel("speed")
+ax.set_xlabel("time")
+ax.set_title("RACE")
+ax.set_xticks([i for i in range(0,10,1)])
+ax.set_yticks([i for i in range(0,100,5)])
+ax.minorticks_off()
+
+
+x=[ i for i in range(0,10,1)]
+y=[random.randint(0,100) for _ in range(10) ]
+line,=ax.plot([],[],marker='o',color='blue')
+line2,=ax.plot([],[],marker='o',color='red')
+y1=[random.randint(0,100) for _ in range(10) ]
+total=100
+step=0
+while step<total:
+    step=step+1
+    line.set_data(x[:step],y[:step])
+    line2.set_data(x[:step],y1[:step])
+
+    plt.draw()
+    plt.pause(.2)
+    
+
